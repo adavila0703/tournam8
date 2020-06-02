6d82ad2362399735c551b95a592d9944b55639de
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2020-06-02 06:43:16 -0500

    Update tournam8.py

--- tournam8.py
+++ tournam8.py
@@ -1,17 +1,21 @@
 import cv2
 import pytesseract
-from discord.ext.commands import Bot, has_permissions, MissingPermissions
+from discord.ext import commands
+from discord.ext.commands import Bot, has_permissions
+from discord.ext.commands.errors import MissingRequiredArgument
 import discord.utils
-from discord import Guild
-from PIL import Image
 import os
-import requests
-import json
-from requests.auth import HTTPBasicAuth
+# from discord import Guild
+# from PIL import Image
+# import requests
+# import json
+# from requests.auth import HTTPBasicAuth
+import time
+import random
 
 bot = Bot(command_prefix='!')
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
 bot.remove_command('help')
 
@@ -56,54 +60,74 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    jokes = ['']
+
     if message.author == bot.user:
         return
 
-    if message.content.startswith('hi'):
-        await message.channel.send('boo')
+    if 'marla' in message.content.lower():
+        await message.channel.send('Yes?')
+
+    if 'best rune' in message.content.lower():
+        await message.channel.send('Blink...')
+
+    if 'blink' in message.content.lower():
+        await message.channel.send('RIP Blink :(')
+
+    if 'marla who created you?' in message.content.lower() or 'marla who created you' in message.content.lower() \
+            or 'who created marla' in message.content.lower() or 'who made marla' in message.content.lower() \
+            or 'who made marla bot' in message.content.lower() or 'who created marla bot' in message.content.lower() \
+            or 'marla who made you?' in message.content.lower() or 'marla who made you' in message.content.lower():
+        await message.channel.send('I was created by marley-EE!')
+
+    if 'tell me a joke' in message.content.lower():
+        print('nothing')
 
 
-    if message.content.startswith('marleybot'):
-        await message.channel.send('BRB')
 
     # OCR
-    if message.attachments:
-        await message.attachments[0].save(str(message.author).split('#')[0] + '.png')
-        image = cv2.imread(str(message.author).split('#')[0] + '.png', 0)
-        thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY_INV)[1]
-        stats = findingstats(pytesseract.image_to_string(thresh, lang='eng', config='--psm 12', nice=1).split())
-        try:
-            await message.channel.send(f'Player: {message.author} \nPlace: {stats[0]} \nExiles: {stats[1]}'
-                                       f'\nAssists: {stats[2]} \nDamage: {stats[3]}')
-        except:
-            pass
-        os.remove(str(message.author).split('#')[0] + '.png')
-        # if str(message.attachments).split()[3].split("'")[1].endswith('.png'):
+    # if message.attachments:
+    #     await message.attachments[0].save(str(message.author).split('#')[0] + '.png')
+    #     image = cv2.imread(str(message.author).split('#')[0] + '.png', 0)
+    #     thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY_INV)[1]
+    #     stats = findingstats(pytesseract.image_to_string(thresh, lang='eng', config='--psm 12', nice=1).split())
+    #     try:
+    #         await message.channel.send(f'Player: {message.author} \nPlace: {stats[0]} \nExiles: {stats[1]}'
+    #                                    f'\nAssists: {stats[2]} \nDamage: {stats[3]}')
+    #     except:
+    #         pass
+    #     os.remove(str(message.author).split('#')[0] + '.png')
+    #     # if str(message.attachments).split()[3].split("'")[1].endswith('.png'):
     await bot.process_commands(message)
 
 
 # Change role on voice channel change
 @bot.event
 async def on_voice_state_update(member, before, after):
-    if str(after.channel) == 'Scrimming Vc':
-        role = discord.utils.get(member.guild.roles, name="badass")
-        await member.add_roles(role)
-    elif str(after.channel) != 'Scrimming Vc':
-        role = discord.utils.get(member.guild.roles, name="badass")
-        await member.remove_roles(role)
+    try:
+        if str(after.channel) == 'Scrimming-VC':
+            role = discord.utils.get(member.guild.roles, name="Player")
+            await member.add_roles(role)
+        elif str(after.channel) != 'Scrimming-VC':
+            role = discord.utils.get(member.guild.roles, name="Player")
+            await member.remove_roles(role)
+    except AttributeError:
+        pass
 
 
 ## BOT COMMANDS
-
 @bot.command()
 @has_permissions(administrator=True)
-async def test(ctx):
-    channel = discord.utils.get(ctx.guild.text_channels, name='marley-ee')
-    await channel.send(file=discord.File('marley.png'))
+async def test(ctx, message):
+    start_time = time.time()
+    await ctx.send("--- %s seconds ---" % (time.time() - start_time))
+    await ctx.send(message)
+
 
 @bot.command()
 @has_permissions(administrator=True)
 async def makescreenshotchannel(ctx, vc_channel, category):
+    start_time = time.time()
     channel = discord.utils.get(ctx.guild.voice_channels, name=vc_channel)
     get_text_channels = ctx.guild.text_channels
     if discord.utils.get(ctx.guild.categories, name=category) is None:
@@ -121,42 +145,41 @@ async def makescreenshotchannel(ctx, vc_channel, category):
             nameout = c.mention
             pic = discord.File('marley.png')
             text_out = discord.utils.get(ctx.guild.text_channels, name=name)
-            await text_out.send(f'Welcome {nameout}! This is the channel where you will be posting your screenshots, '
-                                f'dont forget!\n\nOne of my major features is the ability to read the'
-                                f' information from your screenshot, make sure you get a good picture of your stats'
-                                f' after your game, the better the picture, the more accurate I will be!'
-                                f'\n\nBelow is an and example of the screen you need to capture after your Spellbreak '
-                                f'match!\n\nGood luck today Breaker!\n\n -Marla')
+            await text_out.send(
+                f'Welcome {nameout}! This is the channel where you will be posting your screenshots,'
+                f'dont forget!\n\nOne of my major features is the ability to read the'
+                f' information from your screenshot, make sure you get a good picture of your stats'
+                f' after your game, the better the picture, the more accurate I will be!'
+                f'\n\nBelow is an and example of the screen you need to capture after your '
+                f'Spellbreak '
+                f'match!\n\nGood luck today Breaker!\n\n -Marla')
             await text_out.send(file=discord.File('marley.png'))
             await text_out.send('Tip: ALT + PRINTSCREEN will take a picture of the monitor your mouse is currently'
-                                ' active on. \n\nCTL + v into the discord message will send the picture you just took.')
-
-            file = open('channels.txt', 'r+')
-            file.writelines(str(c).split('#')[0].lower() + ' \n')
-
+                                'active on. \n\nCTL + v into the discord message will send the picture you just '
+                                'took.')
+    await ctx.send(f'!makescreenshotchannel completed - exe time -> {time.time() - start_time}')
 
 
 @bot.command()
 @has_permissions(administrator=True)
-async def deletescreenshotchannels(ctx):
-    channel = ctx.guild.text_channels
-    for c in channel:
-        file = open('channels.txt', 'r+')
-        # for f in file.readlines():
-        #     if f.split()[0] == str(c):
-        #         await
-        #     else:
-        #         pass
+async def deletechannels(ctx, category):
+    start_time = time.time()
+    category = discord.utils.get(ctx.guild.categories, name=category)
+    for c in category.text_channels:
+        await c.delete()
+    await ctx.send(f'!deletechannels completed - exe time -> {time.time() - start_time}')
 
 
 @bot.command()
 @has_permissions(administrator=True, manage_roles=True)
 async def giverole(member, role_name, vc_channel):
+    start_time = time.time()
     fetch = discord.utils.get(member.guild.voice_channels, name=vc_channel)
     user = fetch.members
     role = discord.utils.get(member.guild.roles, name=role_name)
     for u in user:
         await u.add_roles(role)
+    await member.send(f'!giverole completed - exe time -> {time.time() - start_time}')
 
 
 @bot.command()
@@ -164,9 +187,9 @@ async def giverole(member, role_name, vc_channel):
 async def updatescrimroles(member, vc_channel):
     fetch = discord.utils.get(member.guild.voice_channels, name=vc_channel)
     user = fetch.members
-    s1 = discord.utils.get(member.guild.roles, name='scrimmed x1')
-    s2 = discord.utils.get(member.guild.roles, name='scrimmed x2')
-    s3 = discord.utils.get(member.guild.roles, name='scrimmed x3')
+    s1 = discord.utils.get(member.guild.roles, name='Scrimmed 1x')
+    s2 = discord.utils.get(member.guild.roles, name='Scrimmed 2x')
+    s3 = discord.utils.get(member.guild.roles, name='Scrimmed 3x')
     for u in user:
         for r in u.roles:
             if r == s1:
@@ -186,9 +209,9 @@ async def updatescrimroles(member, vc_channel):
 @has_permissions(administrator=True)
 async def removeallscrimroles(member):
     fetch = member.guild.members
-    s1 = discord.utils.get(member.guild.roles, name='scrimmed x1')
-    s2 = discord.utils.get(member.guild.roles, name='scrimmed x2')
-    s3 = discord.utils.get(member.guild.roles, name='scrimmed x3')
+    s1 = discord.utils.get(member.guild.roles, name='Scrimmed 1x')
+    s2 = discord.utils.get(member.guild.roles, name='Scrimmed 2x')
+    s3 = discord.utils.get(member.guild.roles, name='Scrimmed 3x')
     for f in fetch:
         for r in f.roles:
             if r == s1 or r == s2 or r == s3:
@@ -200,14 +223,42 @@ async def removeallscrimroles(member):
 @bot.command()
 @has_permissions(administrator=True, manage_roles=True)
 async def help(member):
-    await member.send("```"
-                   "-!makescreenshotchannel(vc_channel, category) - Creates a text channel for all the users "
-                   "inside a specified voice channel\n\n"
-                   "-!deletescreenshotchannels - Deletes all the screenshot channels that were previously made\n\n"
-                   "-!giverole(role_name, vc_channel) - Give everyone in a specified voice chat a role of choice.\n\n"
-                   "-!updatescrimroles(vc_channel) - Gives a user the 'scrimmed role'(x1, x2, x3) Example: If they have"
-                   "x1 and you run the function, then x1 will be deleted and x2 will be added.\n\n"
-                   "-!removeallscrimroles - Removes all scrim roles from everyone who owns the role, in your channel.\n\n```")
+    await member.send("```Help Menu\nHow to use: !func(p1, p2) will be written out like !func p1 p2, for spaces use"
+                      "quotes!\n\n"
+                      "-!makescreenshotchannel(vc_channel, category) - Creates a text channel for all the users "
+                      "inside a specified voice channel\n\n"
+                      "-!deletechannels(category) - Deletes all text channels in a selected category.\n\n"
+                      "-!giverole(role_name, vc_channel) - Give everyone in a specified voice chat a role of "
+                      "choice.\n\n "
+                      "-!updatescrimroles(vc_channel) - Gives a user the 'scrimmed role'(x1, x2, x3) Example: If they "
+                      "have x1 and you run the function, then x1 will be deleted and x2 will be added.\n\n"
+                      "-!removeallscrimroles - Removes all scrim roles from everyone who owns the role.\n\n"
+                      "** Marla bot was created by marley-EE **```")
+
+
+# BOT Command errors
+@makescreenshotchannel.error
+async def makescreenshotchannel_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Error: You are missing your arguments')
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send('Error: No members in the channel or channel doesnt exist.')
+
+
+@deletechannels.error
+async def deletechannels_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Error: You are missing your arguments')
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send('Error: No channels or category doesnt exist.')
+
+
+# @giverole.error
+# async def giverole_error(ctx, error):
+#     if isinstance(error, commands.MissingRequiredArgument):
+#         await ctx.send('Error: You are missing your arguments')
+#     if isinstance(error, commands.CommandInvokeError):
+#         await ctx.send('Error: No channels or category doesnt exist.')
 
 
 url = "http://localhost:5000/users"
