a50f110490907ae08fc080cfe5a337a915c5a638
Angel Davila <36370758+adavila0703@users.noreply.github.com>
2021-09-24 16:54:42 -0500

    Update tournament_client.py

--- src/client/tournament_client.py
+++ src/client/tournament_client.py
@@ -4,7 +4,6 @@ from requests import Response
 from dotenv import dotenv_values
 
 ENV = dotenv_values('.env')
-proxies = { 'https': 'http://172.19.3.25:8080' }
 
 class TournamentClient:
     def __init__(self, requestor: requests = requests) -> None:
@@ -12,13 +11,13 @@ class TournamentClient:
         self.http_request = requestor
 
     def get_data(self, endpoint: str) -> Response.content:
-        response = self.http_request.get(self.path + endpoint, proxies=proxies)
+        response = self.http_request.get(self.path + endpoint)
         content = json.loads(response.content)
         return response, content
 
     def send_data(self, endpoint: str, body: dict, type: str) -> Response:
         json_body = json.dumps(body)
-        response = self.http_request.post(self.path + endpoint, json_body, proxies=proxies)
+        response = self.http_request.post(self.path + endpoint, json_body)
         print(f'Request Type: { type }, Status Code: { response.status_code }')
         return response
 
